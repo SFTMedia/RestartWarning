ed62a5b8030f8db2f4501b673ecf03832715f8d5
bluetigeresw <eswwilson@gmail.com>
2023-05-05 21:10:10 -0400

    Working! No forced world saving yet, todo feature.

--- src/RestartWarning.cs
+++ src/RestartWarning.cs
@@ -17,8 +17,7 @@ namespace RestartWarning
         {
             // Hook the Chat event to detect when a chat message is sent
             Logger.LogInfo("Loading Restart Warning...");
-            ZRoutedRpc.instance.Register<string, Vector3>("ChatMessage", RPC_ChatMessage);
-            ZRoutedRpc.instance.Register<string, Vector3>("SystemMessage", RPC_ChatMessage);
+
 
             // Set up a timer to trigger at a set time every day
             TimeSpan triggerTime = new TimeSpan(3, 55, 0); // 3:55:00 AM
@@ -29,34 +28,10 @@ namespace RestartWarning
             timer.Start();
         }
 
-        private void SaveWorld()
-        {
-            // Define method for saving the world
-            ZNet.instance.SaveWorld();
-        }
-        private void RPC_ChatMessage(long sender, string message, Vector3 position)
-        {
-            // Check if the chat message contains a specific keyword
-            if (message.Contains("save world"))
-            {
-                if (ZNet.instance != null)
-                {
-                    SaveWorld();
-                }
-                else
-                {
-                    Logger.LogWarning("[RestartWarning] ZNet instance is null, unable to save world.");
-                }
-            }
-        }
-
         private void OnTimerElapsed(object sender, ElapsedEventArgs e)
         {
             // Broadcast a message to all players in the game
             ZRoutedRpc.instance.InvokeRoutedRPC(ZRoutedRpc.Everybody, "ChatMessage", MESSAGE);
-
-            // Save the world
-            SaveWorld();
 
             // Reset the timer for the next trigger
             timer.Interval = TimeSpan.FromDays(1).TotalMilliseconds;
